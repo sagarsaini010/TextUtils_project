24d2c26f2306a0fc0695f9ab9abe81550d08849a
sagarsaini010 <sagarsaini954818@gmail.com>
2025-07-15 21:31:03 +0530

    remove router to deploye application on github

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import { useState } from "react";
 import Alert from "./components/Alert"
-import About from "./components/About";
-import {
-  BrowserRouter as Router,
- Routes,
-  Route,
-} from "react-router-dom";
+// import About from "./components/About";
+// import {
+//   BrowserRouter as Router,
+//  Routes,
+//   Route,
+// } from "react-router-dom";
 function App() {
 
 const [mode , setmode] = useState('light')
@@ -52,20 +52,20 @@ const toggleMode = () =>{
 }
   return (
     <>
-    <Router>
+    {/* <Router> */}
       <Navbar title="TextUtils" about="About Us" mode={mode} toggleMode ={toggleMode} button_mode ={btn_mode} />
       <Alert alert ={alert}/>
       <div className="container my-3">
-         <Routes>
+         {/* <Routes> */}
 
+{/* 
+        <Route exact path="/About Us" element={<About/>} />  */}
+        {/* <Route exact path="/" element={ }/> */}
+<TextForm heading = "Enter the text to analyze below" mode ={mode} alert ={showAlert}/>
 
-        <Route exact path="/About Us" element={<About/>} /> 
-        <Route exact path="/" element={ <TextForm heading = "Enter the text to analyze below" mode ={mode} alert ={showAlert}/>}/>
-
-
-        </Routes>
+        {/* </Routes> */}
       </div>
-   </Router>
+   {/* </Router> */}
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,13 @@
 import React from "react";
 import PropTypes from 'prop-types'
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 export default function Navbar(props) {
   return (
     <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">
+        <a className="navbar-brand" herf="#">
           {props.title}
-        </Link>
+        </a>
         <button
           className="navbar-toggler"
           type="button"
@@ -22,14 +22,14 @@ export default function Navbar(props) {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/">
+              <a className="nav-link active" aria-current="page" herf="#">
                 Home
-              </Link>
+              </a>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/About Us">
+              <a className="nav-link" herf="/About Us">
                 {props.about}
-              </Link>
+              </a>
             </li>
           </ul>
           <form className="d-flex" role="search">
